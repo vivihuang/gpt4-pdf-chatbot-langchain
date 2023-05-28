9cbba3ecb0ebc422005c296878d6ba3d88d7cfd1
Vivi Huang <vivihuang126@gmail.com>
2023-05-28 17:06:37 +0800

    Refactor to make chain call.

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,45 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Chroma } from 'langchain/vectorstores/chroma';
-import { makeChain } from '@/utils/makechain';
-import { CHROMA_NAME_SPACE } from '@/config/chroma';
-import initEmbeddings from "@/utils/embeddings";
+import { makeChain } from '@/utils/makeChain';
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+	req: NextApiRequest,
+	res: NextApiResponse,
 ) {
-  const { question, history } = req.body;
+	const { question, history } = req.body;
 
-  console.log('question', question);
+	console.log('question', question);
 
-  //only accept post requests
-  if (req.method !== 'POST') {
-    res.status(405).json({ error: 'Method not allowed' });
-    return;
-  }
+	//only accept post requests
+	if (req.method !== 'POST') {
+		res.status(405).json({ error: 'Method not allowed' });
+		return;
+	}
 
-  if (!question) {
-    return res.status(400).json({ message: 'No question in the request' });
-  }
-  // OpenAI recommends replacing newlines with spaces for best results
-  const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
+	if (!question) {
+		return res.status(400).json({ message: 'No question in the request' });
+	}
+	// OpenAI recommends replacing newlines with spaces for best results
+	const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
-  try {
-    /* create vectorstore*/
-    const vectorStore = new Chroma(initEmbeddings(), { numDimensions: 1536, collectionName: CHROMA_NAME_SPACE })
-
-    //create chain
-    const chain = makeChain(vectorStore);
-    //Ask a question using chat history
-    const response = await chain.call({
-      question: sanitizedQuestion,
-      chat_history: history || [],
-    });
-
-    console.log('response', response);
-    res.status(200).json(response);
-  } catch (error: any) {
-    console.log('error', error);
-    res.status(500).json({ error: error.message || 'Something went wrong' });
-  }
+	try {
+		const response = await makeChain(sanitizedQuestion, history);
+		console.log('response', response);
+		res.status(200).json(response);
+	} catch (error: any) {
+		console.log('error', error);
+		res.status(500).json({ error: error.message || 'Something went wrong' });
+	}
 }
---
+++ utils/makeChain.ts
@@ -0,0 +1,46 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+import { Chroma } from "langchain/vectorstores/chroma";
+import { ChainValues } from 'langchain/schema'
+import { CallbackManagerForChainRun } from 'langchain/callbacks'
+import initEmbeddings from "@/utils/embeddings";
+import { CHROMA_NAME_SPACE } from "@/config/chroma";
+
+const CONDENSE_PROMPT = `根据以下对话和输入问题，将输入问题改写为一个独立问题。
+
+对话:
+{chat_history}
+输入问题: {question}
+独立问题:`;
+
+const QA_PROMPT = `你是一个来自中国的AI助手。根据下面已知信息，简洁和专业的来回答用户的问题。如果无法从中得到答案，请说 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”，不允许在答案中添加编造成分，答案请使用中文并使用markdown格式。
+
+{context}
+
+问题: {question}
+回答:`;
+
+export const makeChain = async (question: ChainValues, history: CallbackManagerForChainRun) => {
+	const model = new OpenAI({
+		temperature: 0, // increase temepreature to get more creative answers
+		modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+	});
+
+	const embeddings = initEmbeddings()
+
+	const vectorStore = new Chroma(embeddings, { numDimensions: 1536, collectionName: CHROMA_NAME_SPACE })
+
+	const chain = ConversationalRetrievalQAChain.fromLLM(
+		model,
+		vectorStore.asRetriever(),
+		{
+			qaTemplate: QA_PROMPT,
+			questionGeneratorTemplate: CONDENSE_PROMPT,
+			returnSourceDocuments: true, //The number of source documents returned is 4 by default
+		},
+	);
+	return await chain.call({
+		question: question,
+		chat_history: history || [],
+	});
+};
